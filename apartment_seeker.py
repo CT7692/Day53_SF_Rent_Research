5adbde2af14adff2c89593f4f39f2b8d01f7cf6d
Joseph Rydel <jrydel92@gmail.com>
2024-03-27 18:59:21 -0500

    Made changes to fix element interaction issues.

--- apartment_seeker.py
+++ apartment_seeker.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
-from tkinter import messagebox
 from bs4 import BeautifulSoup
 from security import safe_requests
 from time import sleep
